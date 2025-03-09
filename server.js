7e705b41c69834f6a1393212e986f18ab15f0746
Vansh <vanshnagpal0123@gmail.com>
2025-03-09 19:14:03 +0530

    updated image feature

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const mongoURI =process.env.MONGO_URI
 
 app.set('view engine', 'ejs');
